7f6ea613300275acaa45bad1bca4cd2393432413
f4irider02 <grey90stanggt@gmail.com>
2016-10-29 12:25:36 -0600

    Add files via upload

---
+++ ConventionsNSyntax.cs
@@ -0,0 +1,44 @@
+﻿public class ConventionsNSyntax
+{
+    private int health = 100;
+    private float money;
+    public object player;
+
+    public void DealDamage (int damage){
+        health -= damage;
+        if(health < 0){
+            DestroyObject();
+        }
+    }
+
+    public void DestroyObject(){
+        Destroy(player);
+    }
+
+    public float CheckBankAccount(float money){
+        return money;
+    }
+
+    public float Deposit(float myDeposit){
+        money += myDeposit 
+    }
+
+    public float Withdrawal(float myWithdrawal){
+       if(myWithdrawal > money){
+            break;
+        }
+       else{
+            money -= myWithdrawal;
+        }
+    }
+
+
+    public int health(int amount)
+    {
+        health -= amount;
+        return health;
+    }
+}
+
+Deposit(1.50);
+Withdrawal(4);
